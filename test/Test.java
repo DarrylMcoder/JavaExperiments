646f9b8d0beb162191dc503551e3f2ccb32bafd7
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-10-05 21:11:24 -0400

    Update Test.java

--- test/Test.java
+++ test/Test.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+public class Test{
+
+    public static void main(String args[]){
+      Scanner in = new Scanner(System.in);
+	  System.out.println("Enter a number: ");
+	  int a = in.nextInt();
+	  System.out.println("Enter another number: ");
+	  int b = in.nextInt();
+	  int c = a+b;
+	  System.out.println(a + " + " + b + " = " + c);
+    }
+
+}
+
+
 
